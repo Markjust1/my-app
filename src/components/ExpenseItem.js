b95157cddea6a874bae17e29a7d9c87ff0a0ca62
Markjust1 <novikov711@gmail.com>
2022-08-03 21:12:18 -0600

    add component and css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,17 @@
+import './ExpenseItem.css' 
+
 export default function ExpenseItem() {
+  const expenseDate = new Date();
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+
   return (
-    <h2>Expense item!</h2>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
   );
 }
