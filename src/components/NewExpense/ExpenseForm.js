87568823ca8db1ab3da517a8057fcc5a786159db
Markjust1 <novikov711@gmail.com>
2022-09-22 23:48:44 -0600

    add function fix

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [userInput, setUserInput] = useState({
     enteredTitle: "",
     enteredAmount: "",
@@ -32,7 +32,8 @@ const ExpenseForm = () => {
       amount: userInput.enteredAmount,
       date: new Date(userInput.enteredDate),
     };
-    console.log(expenseData);
+    
+    props.onSaveExpenseData(expenseData);
     setUserInput((prev) => {
       return { ...prev, enteredTitle: '' };
     });
